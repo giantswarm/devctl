c3e593eaa8911516cf8fd21942f381bccd0c16aa
Paweł Kopiczko <kopiczko@gmail.com>
2019-06-10 10:06:47 +0100

    bring some latest advancements (#12)
    As this is copy/paste source...
    
    - Add `pkg/project` package.
    - Use `microerror.Stack`.

---
+++ pkg/project/project.go
@@ -0,0 +1,29 @@
+package project
+
+var (
+	description = "Command line tool for the development daily business."
+	gitSHA      = "n/a"
+	name        = "devctl"
+	source      = "https://github.com/giantswarm/devctl"
+	version     = "n/a"
+)
+
+func Description() string {
+	return description
+}
+
+func GitSHA() string {
+	return gitSHA
+}
+
+func Name() string {
+	return name
+}
+
+func Source() string {
+	return source
+}
+
+func Version() string {
+	return version
+}
