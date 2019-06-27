b54626d0ddbd06db173245e40ce41912819502e6
Paweł Kopiczko <kopiczko@gmail.com>
2019-06-27 12:21:22 +0100

    fix imports

--- cmd/gen/command.go
+++ cmd/gen/command.go
@@ -4,10 +4,11 @@ import (
 	"io"
 	"os"
 
-	"github.com/giantswarm/devctl/cmd/gen/resource"
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/micrologger"
 	"github.com/spf13/cobra"
+
+	"github.com/giantswarm/devctl/cmd/gen/resource"
 )
 
 const (
