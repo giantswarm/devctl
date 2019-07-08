dd289450402e17c3714d11bb1553732fb78902eb
Tuomas Mäkinen <tuomas@giantswarm.io>
2019-07-08 12:37:13 +0300

    Add utilization of golden file (and reference from operatorkit)

--- pkg/gen/resource/resource_test.go
+++ pkg/gen/resource/resource_test.go
@@ -1,14 +1,18 @@
 package resource
 
 import (
+	"bytes"
 	"context"
 	"flag"
+	"io/ioutil"
+	"path/filepath"
 	"strconv"
-	"strings"
 	"testing"
+	"unicode"
 
 	"github.com/giantswarm/devctl/pkg/gen/internal"
 	"github.com/giantswarm/microerror"
+	"github.com/google/go-cmp/cmp"
 )
 
 var update = flag.Bool("update", false, "update resource.golden file")
@@ -26,7 +30,7 @@ func Test_Resource(t *testing.T) {
 		errorMatcher func(err error) bool
 	}{
 		{
-			name:         "case 0",
+			name:         "case 0: resources.go",
 			errorMatcher: nil,
 		},
 	}
@@ -35,20 +39,20 @@ func Test_Resource(t *testing.T) {
 		t.Run(strconv.Itoa(i), func(t *testing.T) {
 			ctx := context.Background()
 
-			c := resource.Config{
-				Dir:           "/does/not/matter/for/this/test",
+			c := Config{
+				Dir:           "/go/src/some.domain/project/subpath/configmapresource",
 				ObjectGroup:   "core",
 				ObjectKind:    "ConfigMap",
 				ObjectVersion: "v1",
 			}
 
-			f, err := resource.NewResource(c)
+			f, err := NewResource(c)
 			if err != nil {
-				return microerror.Mask(err)
+				t.Fatal(microerror.Mask(err))
 			}
 
-			var b strings.Builder
-			err := internal.Execute(ctx, w, f)
+			w := &bytes.Buffer{}
+			err = internal.Execute(ctx, w, f)
 
 			switch {
 			case err == nil && tc.errorMatcher == nil:
@@ -61,9 +65,38 @@ func Test_Resource(t *testing.T) {
 				t.Fatalf("error == %#v, want matching", err)
 			}
 
-			if tc.errorMatcher != nil {
-				return
+			actual := w.Bytes()
+
+			golden := filepath.Join("testdata", normalizeToFileName(tc.name)+".golden")
+			if *update {
+				ioutil.WriteFile(golden, actual, 0644)
+			}
+
+			expected, err := ioutil.ReadFile(golden)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if !bytes.Equal(actual, expected) {
+				t.Fatalf("\n\n%s\n", cmp.Diff(actual, expected))
 			}
 		})
 	}
+}
+
+// normalizeToFileName converts all non-digit, non-letter runes in input string
+// to dash ('-'). Coalesces multiple dashes into one.
+func normalizeToFileName(s string) string {
+	var result []rune
+	for _, r := range []rune(s) {
+		if unicode.IsDigit(r) || unicode.IsLetter(r) {
+			result = append(result, r)
+		} else {
+			l := len(result)
+			if l > 0 && result[l-1] != '-' {
+				result = append(result, rune('-'))
+			}
+		}
+	}
+	return string(result)
 }
