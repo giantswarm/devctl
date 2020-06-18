f064d3acd134f269ba232bb0eaf73e6599f659d1
Paweł Kopiczko <kopiczko@gmail.com>
2020-06-18 13:36:27 +0100

    drop --dir flag

--- cmd/gen/workflows/flag.go
+++ cmd/gen/workflows/flag.go
@@ -1,7 +1,6 @@
 package workflows
 
 import (
-	"github.com/giantswarm/microerror"
 	"github.com/spf13/cobra"
 )
 
@@ -10,17 +9,11 @@ const (
 )
 
 type flag struct {
-	Dir string
 }
 
 func (f *flag) Init(cmd *cobra.Command) {
-	cmd.Flags().StringVar(&f.Dir, flagDir, "./.github/workflows", `Directory where the generated code should be located.`)
 }
 
 func (f *flag) Validate() error {
-	if f.Dir == "" {
-		return microerror.Maskf(invalidFlagError, "--%s must not be empty", flagDir)
-	}
-
 	return nil
 }
--- pkg/gen/input/workflows/workflows.go
+++ pkg/gen/input/workflows/workflows.go
@@ -1,15 +1,12 @@
 package workflows
 
 import (
-	"github.com/giantswarm/microerror"
-
 	"github.com/giantswarm/devctl/pkg/gen/input"
 	"github.com/giantswarm/devctl/pkg/gen/input/workflows/internal/file"
 	"github.com/giantswarm/devctl/pkg/gen/input/workflows/internal/params"
 )
 
 type Config struct {
-	Dir string
 }
 
 type Workflows struct {
@@ -17,13 +14,9 @@ type Workflows struct {
 }
 
 func New(config Config) (*Workflows, error) {
-	if config.Dir == "" {
-		return nil, microerror.Maskf(invalidConfigError, "%T.Dir must not be empty", config)
-	}
-
 	w := &Workflows{
 		params: params.Params{
-			Dir: config.Dir,
+			Dir: ".github/workflows",
 		},
 	}
 
