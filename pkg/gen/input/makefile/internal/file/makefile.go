51830156cc1f650dd99190e6e6a53f71101c9daf
axbarsan <mnlk123@yahoo.com>
2020-06-23 20:26:15 +0200

    Add 'v' prefix to package versions

--- pkg/gen/input/makefile/internal/file/makefile.go
+++ pkg/gen/input/makefile/internal/file/makefile.go
@@ -74,16 +74,16 @@ $(APPLICATION)-package-%: $(SOURCES)
 	@$(MAKE) $(APPLICATION)-archive-$*
 
 $(APPLICATION)-rename-binary-%:
-	cp $(APPLICATION)-$* $(APPLICATION)-$(VERSION)-$*-amd64
+	cp $(APPLICATION)-$* $(APPLICATION)-v$(VERSION)-$*-amd64
 
 $(APPLICATION)-archive-%:
 	mkdir -p $(PACKAGE_DIR)
-	tar -cvzf $(APPLICATION)-$(VERSION)-$*-amd64.tar.gz \
-		$(APPLICATION)-$(VERSION)-$*-amd64 \
+	tar -cvzf $(APPLICATION)-v$(VERSION)-$*-amd64.tar.gz \
+		$(APPLICATION)-v$(VERSION)-$*-amd64 \
 		README.md \
 		LICENSE
-	mv $(APPLICATION)-$(VERSION)-$*-amd64.tar.gz $(PACKAGE_DIR)/$(APPLICATION)-$(VERSION)-$*-amd64.tar.gz
-	rm -rf $(APPLICATION)-$(VERSION)-$*-amd64
+	mv $(APPLICATION)-v$(VERSION)-$*-amd64.tar.gz $(PACKAGE_DIR)/$(APPLICATION)-v$(VERSION)-$*-amd64.tar.gz
+	rm -rf $(APPLICATION)-v$(VERSION)-$*-amd64
 
 {{- end}}
 
