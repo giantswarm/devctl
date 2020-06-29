808f98f246a1311b0f3152f034cc0c526a44d052
axbarsan <mnlk123@yahoo.com>
2020-06-29 12:24:27 +0200

    Add 'v' prefix to version tag in built file names

--- pkg/gen/input/makefile/internal/file/makefile.go
+++ pkg/gen/input/makefile/internal/file/makefile.go
@@ -52,28 +52,28 @@ build-darwin: $(APPLICATION)-darwin
 ## build-linux: builds a local binary for linux/amd64
 build-linux: $(APPLICATION)-linux
 
-$(APPLICATION): $(APPLICATION)-$(VERSION)-$(OS)-amd64
+$(APPLICATION): $(APPLICATION)-v$(VERSION)-$(OS)-amd64
 	cp -a $< $@
 
-$(APPLICATION)-darwin: $(APPLICATION)-$(VERSION)-darwin-amd64
+$(APPLICATION)-darwin: $(APPLICATION)-v$(VERSION)-darwin-amd64
 	cp -a $< $@
 
-$(APPLICATION)-linux: $(APPLICATION)-$(VERSION)-linux-amd64
+$(APPLICATION)-linux: $(APPLICATION)-v$(VERSION)-linux-amd64
 	cp -a $< $@
 
-$(APPLICATION)-$(VERSION)-%-amd64: $(SOURCES)
+$(APPLICATION)-v$(VERSION)-%-amd64: $(SOURCES)
 	GOOS=$* GOARCH=amd64 go build -ldflags "$(LDFLAGS)" -o $@ .
 
 {{- if eq .CurrentFlavour .FlavourCLI}}
 
 .PHONY: package-darwin package-linux
 ## package-darwin: prepares a packaged darwin/amd64 version
-package-darwin: $(PACKAGE_DIR)/$(APPLICATION)-$(VERSION)-darwin-amd64.tar.gz
+package-darwin: $(PACKAGE_DIR)/$(APPLICATION)-v$(VERSION)-darwin-amd64.tar.gz
 ## package-linux: prepares a packaged linux/amd64 version
-package-linux: $(PACKAGE_DIR)/$(APPLICATION)-$(VERSION)-linux-amd64.tar.gz
+package-linux: $(PACKAGE_DIR)/$(APPLICATION)-v$(VERSION)-linux-amd64.tar.gz
 
-$(PACKAGE_DIR)/$(APPLICATION)-$(VERSION)-%-amd64.tar.gz: DIR=$(PACKAGE_DIR)/$<
-$(PACKAGE_DIR)/$(APPLICATION)-$(VERSION)-%-amd64.tar.gz: $(APPLICATION)-$(VERSION)-%-amd64
+$(PACKAGE_DIR)/$(APPLICATION)-v$(VERSION)-%-amd64.tar.gz: DIR=$(PACKAGE_DIR)/$<
+$(PACKAGE_DIR)/$(APPLICATION)-v$(VERSION)-%-amd64.tar.gz: $(APPLICATION)-v$(VERSION)-%-amd64
 	mkdir -p $(DIR)
 	cp $< $(DIR)
 	cp README.md LICENSE $(DIR)
