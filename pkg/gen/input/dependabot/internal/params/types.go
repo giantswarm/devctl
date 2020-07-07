f2c2ee032b65dfd5dc70dda852c3377674a98354
Nick Jüttner <nick@giantswarm.io>
2020-07-07 13:41:50 +0200

    Update pkg/gen/input/dependabot/internal/params/types.go
    Co-authored-by: Bricktop <marcel.mueller1@rwth-aachen.de>

--- pkg/gen/input/dependabot/internal/params/types.go
+++ pkg/gen/input/dependabot/internal/params/types.go
@@ -4,6 +4,6 @@ type Params struct {
 	// Dir is the name of the directory where the files of the resource
 	// should be generated.
 	Dir string
-	// Reviewers is a set of people or teams who are assigned as reviewers
+	// Reviewers is a set of people or teams who are assigned as reviewers.
 	Reviewers []string
 }
