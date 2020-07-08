9eeffcd9bf521333f2765c0f25df2e9aa6097426
Nick Jüttner <nick@giantswarm.io>
2020-07-08 11:52:52 +0200

    package formatting

--- cmd/gen/dependabot/flag.go
+++ cmd/gen/dependabot/flag.go
@@ -3,9 +3,10 @@ package dependabot
 import (
 	"strings"
 
-	"github.com/giantswarm/devctl/pkg/gen"
 	"github.com/giantswarm/microerror"
 	"github.com/spf13/cobra"
+
+	"github.com/giantswarm/devctl/pkg/gen"
 )
 
 const (
