3b063dbd9aa48e678f7b4ef19fd670001e10cb8e
GitHub Action <action@github.com>
2020-10-29 09:56:48 +0000

    Bump version to 3.0.1-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "devctl"
 	source      = "https://github.com/giantswarm/devctl"
-	version     = "3.0.0"
+	version     = "3.0.1-dev"
 )
 
 func Description() string {
