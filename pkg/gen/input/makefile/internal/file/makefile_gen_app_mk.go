ec977b04f82593b8e6f968c446e911fa076a220c
Paweł Kopiczko <pawel@giantswarm.io>
2020-11-23 14:01:57 +0000

    Support generating language specific makefiles (#167)

---
+++ pkg/gen/input/makefile/internal/file/makefile_gen_app_mk.go
@@ -0,0 +1,34 @@
+package file
+
+import (
+	"github.com/giantswarm/devctl/pkg/gen/input"
+	"github.com/giantswarm/devctl/pkg/gen/input/makefile/internal/params"
+)
+
+func NewMakefileGenAppMkInput(p params.Params) input.Input {
+	i := input.Input{
+		Path:         "Makefile.gen.app.mk",
+		TemplateBody: makefileGenAppMkTemplate,
+		TemplateData: map[string]interface{}{},
+	}
+
+	return i
+}
+
+var makefileGenAppMkTemplate = `# DO NOT EDIT. Generated with:
+#
+#    devctl gen makefile
+#
+
+.PHONY: lint-chart
+## lint-chart: runs ct against the default chart
+lint-chart: IMAGE := giantswarm/helm-chart-testing:v3.0.0-rc.1
+lint-chart:
+	@echo "====> $@"
+	rm -rf /tmp/$(APPLICATION)-test
+	mkdir -p /tmp/$(APPLICATION)-test/helm
+	cp -a ./helm/$(APPLICATION) /tmp/$(APPLICATION)-test/helm/
+	architect helm template --dir /tmp/$(APPLICATION)-test/helm/$(APPLICATION)
+	docker run -it --rm -v /tmp/$(APPLICATION)-test:/wd --workdir=/wd --name ct $(IMAGE) ct lint --validate-maintainers=false --charts="helm/$(APPLICATION)"
+	rm -rf /tmp/$(APPLICATION)-test
+`
