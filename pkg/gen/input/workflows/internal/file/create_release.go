e2daa81b89dcbc9b7603c54ad73a204f0f35f7f9
Zach Stone <zach@giantswarm.io>
2020-12-01 11:55:34 +0100

    Check for parent tag. Abort if no parent

--- pkg/gen/input/workflows/internal/file/create_release.go
+++ pkg/gen/input/workflows/internal/file/create_release.go
@@ -217,31 +217,37 @@ jobs:
       - name: Create long-lived release branch
         run: |
           current_version="${{ needs.gather_facts.outputs.version }}"
-          parent_version="$(git describe --tags --abbrev=0 HEAD^)"
+          parent_version="$(git describe --tags --abbrev=0 HEAD^ || true)"
           parent_version="${parent_version#v}" # Strip "v" prefix.
+          
+          if [[ "$parent_version" -eq "" ]] ; then
+          echo "Unable to find a parent tag version. No branch to create."
+          exit 0
+          fi
+          
           echo "current_version=$current_version parent_version=$parent_version"
-
+          
           current_major=$(semver get major $current_version)
           current_minor=$(semver get minor $current_version)
           parent_major=$(semver get major $parent_version)
           parent_minor=$(semver get minor $parent_version)
           echo "current_major=$current_major current_minor=$current_minor parent_major=$parent_major parent_minor=$parent_minor"
-
+          
           if [[ $current_major -gt $parent_major ]] ; then
-            echo "Current tag is a new major version"
+          echo "Current tag is a new major version"
           elif [[ $current_major -eq $parent_major ]] && [[ $current_minor -gt $parent_minor ]] ; then
-            echo "Current tag is a new minor version"
+          echo "Current tag is a new minor version"
           else
-            echo "Current tag is not a new major or minor version. Nothing to do here."
-            exit 0
+          echo "Current tag is not a new major or minor version. Nothing to do here."
+          exit 0
           fi
-
+          
           release_branch="release-v${parent_major}.${parent_minor}.x"
           echo "release_branch=$release_branch"
-
+          
           if git rev-parse --verify $release_branch ; then
-            echo "Release branch $release_branch already exists. Nothing to do here."
-            exit 0
+          echo "Release branch $release_branch already exists. Nothing to do here."
+          exit 0
           fi
 
           git branch $release_branch HEAD^
