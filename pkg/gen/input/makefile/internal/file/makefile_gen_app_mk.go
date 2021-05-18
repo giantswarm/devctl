b247fc7bbc99b23fd1dd5c93496ac67a44fca1e4
Marian Steinbach <marian@giantswarm.io>
2021-05-18 13:26:14 +0200

    Use embed for templates

--- pkg/gen/input/makefile/internal/file/makefile_gen_app_mk.go
+++ pkg/gen/input/makefile/internal/file/makefile_gen_app_mk.go
@@ -1,9 +1,14 @@
 package file
 
 import (
+	_ "embed"
+
 	"github.com/giantswarm/devctl/pkg/gen/input"
 	"github.com/giantswarm/devctl/pkg/gen/input/makefile/internal/params"
 )
+
+//go:embed Makefile.gen.app.mk.template
+var makefileGenAppMkTemplate string
 
 func NewMakefileGenAppMkInput(p params.Params) input.Input {
 	i := input.Input{
@@ -16,19 +21,3 @@ func NewMakefileGenAppMkInput(p params.Params) input.Input {
 
 	return i
 }
-
-var makefileGenAppMkTemplate = `{{ .Header }}
-
-##@ App
-
-.PHONY: lint-chart
-lint-chart: IMAGE := giantswarm/helm-chart-testing:v3.0.0-rc.1
-lint-chart: ## Runs ct against the default chart.
-	@echo "====> $@"
-	rm -rf /tmp/$(APPLICATION)-test
-	mkdir -p /tmp/$(APPLICATION)-test/helm
-	cp -a ./helm/$(APPLICATION) /tmp/$(APPLICATION)-test/helm/
-	architect helm template --dir /tmp/$(APPLICATION)-test/helm/$(APPLICATION)
-	docker run -it --rm -v /tmp/$(APPLICATION)-test:/wd --workdir=/wd --name ct $(IMAGE) ct lint --validate-maintainers=false --charts="helm/$(APPLICATION)"
-	rm -rf /tmp/$(APPLICATION)-test
-`
