c97cddd799b6228fc261887397635ec0fd3074d5
Jihyuk Bok <jhbok@giantswarm.io>
2021-08-09 19:05:10 +0200

    add renovate command

--- cmd/gen/renovate/runner.go
+++ cmd/gen/renovate/runner.go
@@ -1,1 +1,67 @@
 package renovate
+
+import (
+	"context"
+	"io"
+
+	"github.com/giantswarm/devctl/pkg/gen/input/renovate"
+	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/micrologger"
+	"github.com/spf13/cobra"
+
+	"github.com/giantswarm/devctl/pkg/gen"
+	"github.com/giantswarm/devctl/pkg/gen/input"
+)
+
+type runner struct {
+	flag   *flag
+	logger micrologger.Logger
+	stdout io.Writer
+	stderr io.Writer
+}
+
+func (r *runner) Run(cmd *cobra.Command, args []string) error {
+	ctx := context.Background()
+
+	err := r.flag.Validate()
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	err = r.run(ctx, cmd, args)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	return nil
+}
+
+func (r *runner) run(ctx context.Context, cmd *cobra.Command, args []string) error {
+	var err error
+
+	var renovateInput *renovate.Renovate
+	{
+		c := renovate.Config{
+			Interval: r.flag.Interval,
+			Language: r.flag.Language,
+			Reviewer: r.flag.Reviewer,
+		}
+
+		renovateInput, err = renovate.New(c)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+	}
+
+	var inputs []input.Input
+	{
+		inputs = append(inputs, renovateInput.CreateRenovate())
+	}
+
+	err = gen.Execute(ctx, inputs...)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	return nil
+}
