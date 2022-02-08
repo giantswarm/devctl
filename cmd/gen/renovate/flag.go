f2ca37b2ee36a46a899b4ec7d1ab1b75f772d658
Jose Armesto <github@armesto.net>
2022-02-08 16:34:14 +0100

    Schedule renovate earlier

--- cmd/gen/renovate/flag.go
+++ cmd/gen/renovate/flag.go
@@ -18,7 +18,7 @@ type flag struct {
 }
 
 func (f *flag) Init(cmd *cobra.Command) {
-	cmd.Flags().StringVarP(&f.Interval, flagInterval, "i", "after 9am on thursday", "Check for daily, weekly or monthly updates.")
+	cmd.Flags().StringVarP(&f.Interval, flagInterval, "i", "after 6am on thursday", "Check for daily, weekly or monthly updates.")
 	cmd.Flags().StringVarP(&f.Language, flagLanguage, "l", "", "Language for Renovate to  monitor for new versions , e.g. go, docker.")
 	cmd.Flags().StringVarP(&f.Reviewer, flagReviewer, "r", "", "Reviewer you want to assign automatically when Renovate creates a PR, e.g. giantswarm/team-firecracker.")
 }
