f8badf39fb06d226ed324b3f399e163f1ac548ca
Jose Armesto <github@armesto.net>
2022-04-04 12:12:19 +0200

    Fix after forgetting to merge this

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "devctl"
 	source      = "https://github.com/giantswarm/devctl"
-	version     = "4.24.2-dev"
+	version     = "5.0.1-dev"
 )
 
 func Description() string {
