a807422371883d0992d2de99e88c8d3029d1d3ca
GitHub Action <action@github.com>
2022-04-12 12:30:27 +0000

    Bump version to 5.1.2-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "devctl"
 	source      = "https://github.com/giantswarm/devctl"
-	version     = "5.1.1"
+	version     = "5.1.2-dev"
 )
 
 func Description() string {
