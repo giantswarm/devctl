7fe7dde6729ab7d307f4ecd73a2bf5f0b5802ab3
Marian Steinbach <marian@giantswarm.io>
2023-09-22 14:33:04 +0200

    Make --interval flag optional, remove default value

--- cmd/gen/renovate/flag.go
+++ cmd/gen/renovate/flag.go
@@ -16,14 +16,11 @@ type flag struct {
 }
 
 func (f *flag) Init(cmd *cobra.Command) {
-	cmd.Flags().StringVarP(&f.Interval, flagInterval, "i", "after 6am on thursday", "Check for daily, weekly or monthly updates.")
+	cmd.Flags().StringVarP(&f.Interval, flagInterval, "i", "", "Check for daily, weekly or monthly updates.")
 	cmd.Flags().StringVarP(&f.Language, flagLanguage, "l", "", "Language for Renovate to  monitor for new versions , e.g. go, docker.")
 }
 
 func (f *flag) Validate() error {
-	if f.Interval == "" {
-		return microerror.Maskf(invalidFlagError, "--%s cannot be empty", flagInterval)
-	}
 	if f.Language == "" {
 		return microerror.Maskf(invalidFlagError, "--%s cannot be empty", flagLanguage)
 	}
