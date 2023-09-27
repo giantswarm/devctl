28245539b03e927bfe9f091649135b7ad4aef0a3
Marian Steinbach <marian@giantswarm.io>
2023-09-27 16:06:54 +0200

    Shorten flag descriptions

--- cmd/repo/setup/flag.go
+++ cmd/repo/setup/flag.go
@@ -39,20 +39,20 @@ func (f *flag) Init(cmd *cobra.Command) {
 	cmd.PersistentFlags().StringVar(&f.GithubTokenEnvVar, "github-token-envvar", "GITHUB_TOKEN", "Environment variable name for Github token.")
 
 	// Features
-	cmd.PersistentFlags().BoolVar(&f.EnableWiki, "enable-wiki", false, "Either true to enable the wiki for this repository or false to disable it.")
-	cmd.PersistentFlags().BoolVar(&f.EnableIssues, "enable-issues", true, "Either true to enable issues for this repository or false to disable them.")
-	cmd.PersistentFlags().BoolVar(&f.EnableProjects, "enable-projects", false, "Either true to enable projects for this repository or false to disable them.")
-	cmd.PersistentFlags().BoolVar(&f.Archived, "archived", false, "true to archive this repository.")
+	cmd.PersistentFlags().BoolVar(&f.EnableWiki, "enable-wiki", false, "Enable wiki for this repo, false to remove it.")
+	cmd.PersistentFlags().BoolVar(&f.EnableIssues, "enable-issues", true, "Enable issues for this repo, or false to remove them.")
+	cmd.PersistentFlags().BoolVar(&f.EnableProjects, "enable-projects", false, "Enable projects for this repo, or false to remove them.")
+	cmd.PersistentFlags().BoolVar(&f.Archived, "archived", false, "Mark this repo as archived.")
 
 	// Merge settings
-	cmd.PersistentFlags().BoolVar(&f.AllowMergeCommit, "allow-mergecommit", false, "Either true to allow merging pull requests with a merge commit, or false to prevent merging pull requests with merge commits.")
-	cmd.PersistentFlags().BoolVar(&f.AllowSquashMerge, "allow-squashmerge", true, "Either true to allow squash-merging pull requests, or false to prevent squash-merging.")
-	cmd.PersistentFlags().BoolVar(&f.AllowRebaseMerge, "allow-rebasemerge", false, "Either true to allow rebase-merging pull requests, or false to prevent rebase-merging.")
+	cmd.PersistentFlags().BoolVar(&f.AllowMergeCommit, "allow-mergecommit", false, "Allow merging pull requests with a merge commit, or false to prevent it.")
+	cmd.PersistentFlags().BoolVar(&f.AllowSquashMerge, "allow-squashmerge", true, "Allow squash-merging pull requests, or false to prevent it.")
+	cmd.PersistentFlags().BoolVar(&f.AllowRebaseMerge, "allow-rebasemerge", false, "Allow rebase-merging pull requests, or false to prevent it.")
 
 	// Pull requests
 	cmd.PersistentFlags().BoolVar(&f.AllowUpdateBranch, "allow-updatebranch", true, "Whenever there are new changes available in the base branch, present an “update branch” option in the pull request.")
-	cmd.PersistentFlags().BoolVar(&f.AllowAutoMerge, "allow-automerge", true, "Either true to allow auto-merge on pull requests, or false to disallow auto-merge.")
-	cmd.PersistentFlags().BoolVar(&f.DeleteBranchOnMerge, "delete-branch-on-merge", true, "Either true to allow automatically deleting head branches when pull requests are merged, or false to prevent automatic deletion.")
+	cmd.PersistentFlags().BoolVar(&f.AllowAutoMerge, "allow-automerge", true, "Allow auto-merge on pull requests, or false to forbid it.")
+	cmd.PersistentFlags().BoolVar(&f.DeleteBranchOnMerge, "delete-branch-on-merge", true, "Automatically delete head branches when PRs are merged, or false to prevent it.")
 
 	// Permissions
 	cmd.PersistentFlags().StringToStringVar(&f.Permissions, "permissions", map[string]string{"Employees": "admin", "bots": "push"}, "Grant access to this repository using github_team_name=permission format. Multiple values can be provided as a comma separated list or using this flag multiple times. Permission can be one of: pull, push, admin, maintain, triage.")
