62d8ec3b8e85a899381e2d1acb1eb4a67c1947fd
taylorbot <dev@giantswarm.io>
2023-11-28 13:01:42 +0000

    Bump version to 6.17.3-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "devctl"
 	source      = "https://github.com/giantswarm/devctl"
-	version     = "6.17.2"
+	version     = "6.17.3-dev"
 )
 
 func Description() string {
