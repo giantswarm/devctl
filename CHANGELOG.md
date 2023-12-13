acd4439b4c67fe9fcf9afee2d156dd31d8bc9dbd
Franco <franco@giantswarm.io>
2023-12-13 08:57:58 -0300

    Add Fix Vulnerabilities workflow for Nancy findings remediation

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Added
+
+- Add a `Fix Vulnerabilities` workflow to remediate Nancy findings.
+
 ## [6.18.2] - 2024-01-19
 
 ### Changed
--- cmd/gen/workflows/flag.go
+++ cmd/gen/workflows/flag.go
@@ -14,6 +14,7 @@ const (
 	flagCheckSecrets                   = "check-secrets"
 	flagEnableFloatingMajorVersionTags = "enable-floating-major-tags"
 	flagFlavour                        = "flavour"
+	flagLanguage                       = "language"
 	flagInstallUpdateChart             = "install-update-chart"
 	flagRunSecurityScorecard           = "run-security-scorecard"
 )
@@ -22,6 +23,7 @@ type flag struct {
 	CheckSecrets                   bool
 	EnableFloatingMajorVersionTags bool
 	Flavours                       gen.FlavourSlice
+	Language                       string
 	InstallUpdateChart             bool
 	RunSecurityScorecard           bool
 }
@@ -30,6 +32,7 @@ func (f *flag) Init(cmd *cobra.Command) {
 	cmd.Flags().BoolVar(&f.CheckSecrets, flagCheckSecrets, true, "If true, also generate a secret-scanning workflow. Possible values: true (default), false.")
 	cmd.Flags().BoolVar(&f.EnableFloatingMajorVersionTags, flagEnableFloatingMajorVersionTags, false, "If true, also generate steps and workflows to ensure floating major version tags like \"v1\" after the release creation.")
 	cmd.Flags().VarP(gen.NewFlavourSliceFlagValue(&f.Flavours, gen.FlavourSlice{}), flagFlavour, "f", fmt.Sprintf(`The type of project that you want to generate the workflows for. Possible values: <%s>`, strings.Join(gen.AllFlavours(), "|")))
+	cmd.Flags().StringVarP(&f.Language, flagLanguage, "l", "", "Language of your repo. If go, also generate a fix_vulnerabilities workflow.")
 	cmd.Flags().BoolVar(&f.InstallUpdateChart, flagInstallUpdateChart, false, "If true, also generate update_chart workflow. Only valid for app flavor.")
 	cmd.Flags().BoolVar(&f.RunSecurityScorecard, flagRunSecurityScorecard, true, "If true, also generate a security scorecard workflow. Possible values: true (default), false.")
 }
