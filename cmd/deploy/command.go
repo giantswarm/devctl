f68a4877832c8de7951c43dfbedafa57635e6663
pipo02mix <fernando@giantswarm.io>
2025-04-03 16:51:14 +0200

    Add example command

--- cmd/deploy/command.go
+++ cmd/deploy/command.go
@@ -1,11 +1,30 @@
 package deploy
 
 import (
+	"fmt"
 	"io"
 
 	"github.com/giantswarm/microerror"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
+)
+
+const (
+	name             = "deploy"
+	shortDescription = `Deploy an application to a GitOps repository`
+	longDescription  = `Deploy an application to a GitOps repository by:
+		1. Cloning the GitOps repository
+		2. Adding the application using 'kubectl gs gitops add app'
+		3. Creating a branch and committing the changes
+		4. Creating a pull request
+		5. Check the app is finally deployed`
+	example = `  devctl deploy --gitops-repo giantswarm/workload-clusters-fleet \
+		--app-name hello-world \
+		--app-version 2.8.1 \
+		--app-catalog giantswarm \
+		--workload-cluster operations \
+		--organization giantswarm-production \
+		--target-namespace default`
 )
 
 type Config struct {
@@ -35,28 +54,11 @@ func New(config Config) (*cobra.Command, error) {
 	}
 
 	c := &cobra.Command{
-		Use:   "deploy",
-		Short: "Deploy an application to a GitOps repository",
-		Long: `Deploy an application to a GitOps repository by:
-		1. Cloning the GitOps repository
-		2. Adding the application using 'kubectl gs gitops add app'
-		3. Creating a branch and committing the changes
-		4. Creating a pull request
-		5. Check the app is finally deployed
-
-Example:
-    devctl deploy \
-        --gitops-repo giantswarm/workload-clusters-fleet \
-        --app-name hello-world \
-        --app-version 0.1.0 \
-        --app-catalog giantswarm \
-        --workload-cluster demo \
-        --organization giantswarm-production \
-        --target-namespace default`,
-
-		RunE:          r.Run,
-		SilenceErrors: true,
-		SilenceUsage:  true,
+		Use:     fmt.Sprintf("%s [--gitops-repo] REPOSITORY", name),
+		Short:   shortDescription,
+		Long:    longDescription,
+		Example: example,
+		RunE:    r.Run,
 	}
 
 	f.Init(c)
