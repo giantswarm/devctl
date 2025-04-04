83885eaf382736e49fd402e9da97ccf0c937ecba
pipo02mix <fernando@giantswarm.io>
2025-04-04 16:40:51 +0200

    Remove unused error

--- pkg/appstatus/error.go
+++ pkg/appstatus/error.go
@@ -5,7 +5,3 @@ import "github.com/giantswarm/microerror"
 var invalidConfigError = &microerror.Error{
 	Kind: "invalidConfigError",
 }
-
-var executionFailedError = &microerror.Error{
-	Kind: "executionFailedError",
-}
