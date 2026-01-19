222f22de8f3002bf022ccacd27201ff3fc1ea89a
taylorbot <dev@giantswarm.io>
2026-01-19 12:47:23 +0000

    Release v7.28.0

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "devctl"
 	source      = "https://github.com/giantswarm/devctl"
-	version     = "7.27.1-dev"
+	version     = "7.28.0"
 )
 
 func Description() string {
