abcbf90dc87521b2ef37f0cafb28bd3d86f3ad9d
taylorbot <dev@giantswarm.io>
2026-06-03 06:02:18 +0000

    chore: bump version to 8.3.5-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "devctl"
 	source      = "https://github.com/giantswarm/devctl"
-	version     = "8.3.4"
+	version     = "8.3.5-dev"
 )
 
 func Description() string {
