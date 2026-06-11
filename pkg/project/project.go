4597bf770b3d17ae085b1dc8bb63cecc4e6a85af
taylorbot <dev@giantswarm.io>
2026-06-11 11:58:08 +0000

    chore(release): v8.13.0
    Release-Workflow-Run: 27345094271

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "devctl"
 	source      = "https://github.com/giantswarm/devctl"
-	version     = "8.12.0"
+	version     = "8.13.0"
 )
 
 func Description() string {
